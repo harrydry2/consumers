b7f7bd166e448b432b3fe7aaf6b79d2f3bb87f16
harrydry <harrydry1996@gmail.com>
2018-01-08 11:27:25 +0000

    s

--- public/src/index.js
+++ public/src/index.js
@@ -1,6 +1,7 @@
 import "./sass/styles.sass";
-import { $, $$ } from "./modules/bling";
-import elements from "./modules/elements";
-import "./modules/modal";
-import "./modules/blockFormSubmit";
-import "./modules/coupon";
+// import { $, $$ } from "./modules/bling";
+// import elements from "./modules/elements";
+// import "./modules/modal";
+// import "./modules/blockFormSubmit";
+// import "./modules/coupon";
+// import "./modules/typed";
---
+++ public/src/modules/typed.js
@@ -0,0 +1,8 @@
+// Run typed.js
+$(document).ready(function() {
+  $("#typed").typed({
+    stringsElement: $("#typed-strings"),
+    startDelay: 2000,
+    typeSpeed: 1.7
+  });
+});
