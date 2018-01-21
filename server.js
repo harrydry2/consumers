cb1d385e7688ec8f1fd196d1ba49dfa7b22ac08a
harrydry <harrydry1996@gmail.com>
2018-01-21 12:33:55 +0000

    s

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.locals.format = require("date-fns/format");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.json());
